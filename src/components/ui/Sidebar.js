c0c2e8f50355d59e016d8f585799fea1c462f308
Scarlet Angelina Espinoza Moreno <scanespi@espol.edu.ec>
2021-08-11 21:24:46 -0500

    cambios pequeños

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -4,16 +4,16 @@ import {NavLink} from 'react-router-dom';
 const Sidebar = () => {
     return (
         
-         <div className="md:w-2/5 xl:w-1/5 bg-blue-800">
+         <div className="md:w-2/5 xl:w-1/6 bg-blue-800">
              <div className="p-6">
                 <p className="uppercase text-white text-2xl tracking-wide text-center font-bold"> Hosteria</p>
                 <p className="mt-3 text-black-600">:</p>
              
                 <nav className="mt-10">
-                    <NavLink className="p-1 text-gray-400 block hover:bg-gray-500 hover: text-gray-500" activeClassName ="text-white-500" exact="true" to="/Habitaciones">Habitaciones</NavLink>
-                    <NavLink className="p-1 text-gray block hover:bg-gray-500 hover: text-gray-500" activeClassName="text-white-500" exact="true" to="/promociones">Promociones</NavLink>
-                    <NavLink className="p-1 text-gray block hover:bg-gray-500 hover: text-gray-500" activeClassName="text-white-500" exact="true" to="/actividades">Actividades</NavLink> 
-                    <NavLink className="p-1 text-gray block hover:bg-gray-500 hover: text-gray-500" activeClassName="text-white-500" exact="true" to="/">Reservas</NavLink> 
+                    <NavLink className="p-1 text-white block hover:bg-gray-600 hover: text-white-500" activeClassName ="text-white-500" exact="true" to="/Habitaciones">Habitaciones</NavLink>
+                    <NavLink className="p-1 text-white block hover:bg-gray-600 hover: text-white-500" activeClassName="text-white-500" exact="true" to="/promociones">Promociones</NavLink>
+                    <NavLink className="p-1 text-white block hover:bg-gray-600 hover: text-white-500" activeClassName="text-white-500" exact="true" to="/actividades">Actividades</NavLink> 
+                    <NavLink className="p-1 text-white block hover:bg-gray-600 hover: text-white-500" activeClassName="text-white-500" exact="true" to="/">Reservas</NavLink> 
 
                      
                 </nav>
